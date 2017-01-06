d89e8cdda4eb8a4d7fc7174a9029226d7498c1af
jsnwyse <jsnwyse@gmail.com>
2017-01-06 15:18:19 +0000

    Add files via upload

--- R/blca.gibbs.R
+++ R/blca.gibbs.R
@@ -1,209 +1,170 @@
-blca.gibbs <-
-function(X,G, alpha=1, beta=1, delta=1, start.vals= c("prior","single","across"), counts.n=NULL, iter=5000, thin=1, accept=thin, burn.in=100, relabel=TRUE, verbose=TRUE, verbose.update=1000)
+# this is the Gibbs sampler and needs to be renamed
+
+blca.gibbs <- function( X , G, alpha = 1, beta = 1, delta = 1, num.categories = NULL, iter = 5000, burn.in = 100, thin = 1, relabel = TRUE, verbose = TRUE, verbose.update = 1000 )
 {
-	if(is.null(counts.n))
+
+	t1 <- proc.time()
+
+	X <- as.matrix(X)
+
+	N <- nrow(X)
+	M <- ncol(X)
+	
+	#check that matrix is binary and/or n.categories is passed
+	if( is.null(num.categories) ){
+		if( !all( X[X>0]==1) ){
+			stop("\t A matrix other than binary must have non-null n.categories vector" )
+		} else {
+			#matrix is binary
+			num.categories <- rep( 2, M )
+		}
+	}
+
+	t <- apply( X, 2, min )
+	if( sum(t) > 0 ) stop("\t please recode categories from 0,...,ncat  to use blca.collapsed")
+	t <- apply( X, 2, max )
+	if( sum( t + 1 - num.categories ) > 0 ) stop("\n number of categories in X exceeds num.categories please recode categories from 0, ..., ncat.")
+	
+	## safety checks ##
+	
+	if(length(num.categories)!= M){
+		stop("\t The length of n.categories must be the same as the number of records per observation.")
+	}	
+
+	if(iter < burn.in){
+		warning("\t The number of burn in iterations is greater than the number of iterations-- this will be automatically adjusted to the functions parameters.")
+		iter = iter + burn.in
+	}
+
+	#if((iter-burn.in)%%thin != 0)
+	#	stop("\t Please thin by an amount that divides perfectly into iter - burn.in. ")
+
+	stored <- iter / thin
+
+	memberships = numeric(stored*N)
+	weights = numeric(stored*G)
+	variable.probs = numeric(stored*G*sum(num.categories))
+	log.post = numeric( stored )
+	
+	model.indicator <- rep(1,M) #modify this later to allow for flexible model specification
+
+	hparam <- c( delta, beta )
+
+	w <- .C(	"BLCA_GIBBS_SAMPLER",													as.integer(X),
+				as.integer(N),																as.integer(M),
+				as.integer(num.categories),											as.double(hparam),
+				as.integer(G),															 	as.integer(iter),											
+				as.integer(burn.in),
+				as.integer(thin),															memberships = as.integer(memberships),					
+				weights = as.double(weights),											variable.probs = as.double(variable.probs),			
+				log.posterior = as.double(log.post),								as.integer(model.indicator), 
+				as.integer( verbose ),													as.integer( verbose.update ),			
+				PACKAGE = "BayesLCA" )
+
+	membership.mat <- matrix( w$memberships, nrow = stored, ncol=N, byrow=FALSE ) + 1
+	
+	#split these into a list as it will be easier to rearrange from label processing
+	var.probs.l <- list()
+	
+	k <- 1
+	for( i in 1:M )
 	{
-		if(inherits(X,"data.blca")){
-			counts.n<- X$counts.n
-			X<- X$data
+		if(i == 1){
+			gap <- 0
 		}else{
-			Xdat<- data.blca(X)
-			X<- Xdat$data
-			counts.n<- Xdat$counts.n
-			}# else class(X)
-		} else{ 
-			X<- as.matrix(X)
-			if(any(X[X>0]!=1))
-			stop("If vector of counts is supplied separately, then data must be binary.")
-			}
-	
-	M<-ncol(X); N<-nrow(X)
-	if(2^M <= (M+1)*G) warning(paste("Model may be improperly specified. Maximum number of classes that should be run is", floor(2^M/(M+1)), "."))
-	
-		if(length(delta)==1) delta<-rep(delta,G)
-		if(length(delta)!=G) stop("delta prior is wrong length (i.e., !=1 or G)")
-			if(!is.matrix(alpha)){
-			if(any(length(alpha)==c(1,G)) ){
-				alpha<-matrix(alpha,G,M)
-			} else {
-				if(length(alpha)==M){
-					alpha<- matrix(alpha,G,M, byrow=TRUE)
-				} else stop("Item probability prior improperly specified.")
-			}
-	} #else {
-	#	if(!is.matrix(alpha)) stop("Item probability prior improperly specified.")
-	#}	
-	
-	if(!is.matrix(beta)){
-		if(any(length(beta)==c(1,G)) ){
-			beta<-matrix(beta,G,M)
-			} else {
-				if(length(beta)==M){
-					beta<- matrix(beta,G,M, byrow=TRUE)
-				} else stop("Item probability prior improperly specified.")
-			}
-		} #else {
-			#if(!is.matrix(beta)) stop("Item probability prior improperly specified.")
-	#}	
-	if(any(dim(alpha)!=c(G,M), dim(beta)!=c(G,M))) stop("Item probability prior improperly specified.")
-
-
-	
-	#Prior
-	tau<- rep(0,G)
-	theta<- matrix(0,G,M)
-	if(is.character(start.vals)){
-	  if(start.vals[1]=="prior"){
-	    tau<-rdirichlet(1,delta)
-	    theta<-matrix(rbeta(G*M,alpha,beta),nrow=G,ncol=M)
-	    }else{
-	  if(start.vals[1]=="single"){
-	    Z<-unMAP(sample(1:G,size=N,replace=TRUE))
-	    if(ncol(Z)<G) Z<-cbind(Z, matrix(0,nrow=N, ncol=(G-ncol(Z))))
-	    tau<-rdirichlet(1,delta+colSums(Z))
-	    for(g in 1:G) theta[g,]<-rbeta(M,alpha+colSums(Z[,g]*X), beta+colSums(Z[,g]*(1-X)))
-	  }else{
-	    if(start.vals[1]=="across"){
-		Z<- matrix(runif(N*G), N,G)
-		Z<- Z/rowSums(Z)
-		tau<-rdirichlet(1,delta+colSums(Z))
-		for(g in 1:G) theta[g,]<-rbeta(M,alpha+colSums(Z[,g]*X), beta+colSums(Z[,g]*(1-X)))
-		} else stop("start.vals improperly specified. See help files for more details.")
-	      }
-	     }
-	    } else{
-	      if(is.matrix(start.vals) & all(dim(as.matrix(start.vals)) == c(N,G))){
-		Z<- start.vals 
-		tau<-rdirichlet(1,delta+colSums(Z))
-		for(g in 1:G) theta[g,]<-rbeta(M,alpha+colSums(Z[,g]*X), beta+colSums(Z[,g]*(1-X)))
-	      }else{
-		  if(is.numeric(start.vals) & length(as.numeric(start.vals))==N){ 
-		    Z<- unMAP(start.vals)
-		    tau<-rdirichlet(1,delta+colSums(Z))
-		    for(g in 1:G) theta[g,]<-rbeta(M,alpha+colSums(Z[,g]*X), beta+colSums(Z[,g]*(1-X)))
-		    } else stop("start.vals improperly specified. See help files for more details.")
-		 }
-	  }	
-
-	#Store Values
-	maxiter<- iter
-	if(accept!=thin) thin<- accept
-	Kstore<-maxiter*thin
-	taustore<-matrix(NA,Kstore,G)
-	thetastore<-array(NA,c(Kstore,G,M))
-	labelstore<-matrix(NA, Kstore,G)
-	logpost.store<- rep(NA, Kstore)
-	counter<-1
-	#Dummy matrix
-	W<-matrix(nrow=N, ncol=G)
-	Zstore<-matrix(0, N, G)
-	label.swap<- FALSE
-
-	if(verbose==TRUE) cat("Initialising sampler...starting burn-in.\n")
-	#Gibbs Sampler
-	for (iter in 1:(maxiter+burn.in))
-	{
-		for(g in 1:G)	W[,g]<-tau[g]*apply(theta[g,]^t(X)*(1-theta[g,])^t(1-X),2,prod) # g
-	
-		Z<-Zsamp(W, counts.n) 
-
-		if(iter==burn.in+1){
-		  if(verbose==TRUE) cat("Burn-in completed...\n")
-		  Zmatch<- counts.n*Z
-		  }
-		
-		tau<-rdirichlet(1,delta+colSums(Z))
-
-		for(g in 1:G) theta[g,]<-rbeta(M,alpha+colSums(Z[,g]*X), beta+colSums(Z[,g]*(1-X)))
-
-		if((iter>burn.in)&&(iter%%round(1/thin)==0))
+			gap <- stored * G * sum( num.categories[1:(i-1)] )
+		}
+		#variable probabilities stacked by group and then iteration
+		if( model.indicator[i] )
 		{
-			if(verbose==TRUE & (iter-burn.in)%%verbose.update == 0) cat(iter - burn.in, "of", maxiter, "samples completed...\n")
-			if(G > 1) { 
-				match1<- matchClasses(t(Z)%*%Zmatch, method="exact", verbose=FALSE) } else match1 <- 1:G
-			if(any(match1!=1:G)) label.swap<- TRUE
-			labelstore[counter,]<- match1
-			if(relabel){
-				taustore[counter,labelstore[counter,]]<-tau
-				thetastore[counter, labelstore[counter,], ]<-theta
-				Zstore<- ((iter-burn.in-1)*Zstore + Z[, labelstore[counter,]])/(iter - burn.in)				
-			}else{
-				taustore[counter, ]<-tau
-				thetastore[counter, , ]<-theta
-				Zstore<- ((iter-burn.in-1)*Zstore + Z)/(iter - burn.in)	
+			var.probs.l[[k]] <- matrix( w$variable.probs[(gap+1):(gap + stored*G*num.categories[i])] , nrow = stored * G, ncol=num.categories[i], byrow=TRUE )
+			k <- k+1
+		}
+	}	
+	
+	weights.mat <- matrix( w$weights , nrow=stored, ncol=G, byrow=TRUE )
+
+	if(relabel) relabelled <- undo.label.switching( membership.mat,rep(G, stored) )
+	
+	#post processed weights and probabilities
+	
+	l <- 1
+	for( k in 1:stored )
+	{
+		weights.mat[ k, ] <- weights.mat[ k, relabelled$permutation[k,1:G] ]
+		for( j in 1:M  )
+		{
+			it <- (k-1)*G
+			if( model.indicator[j] )
+			{
+				var.probs.l[[l]][ it + 1:G , ] = var.probs.l[[j]][ it + relabelled$permutation[ k, 1:G ] , ]
+				l <- l+1
 			}
-			
-			logpost.store[counter]<- sum(log(rowSums(W))*counts.n) + sum(xlogy(alpha-1, theta) + xlogy(beta-1, 1-theta) + sum(xlogy(delta-1, tau)))
-			
-			counter<-counter+1
-			if(counter>Kstore) break
-			}
-		} #iter
-	if(verbose==TRUE) cat("Sampling run completed.\n")
-	tau<- apply(taustore, 2, mean)
-	o<- order(tau, decreasing=TRUE)
-
-	x<-NULL
-	x$call<- match.call()
-	x$classprob<- tau[o]
-	x$itemprob<- apply(thetastore, c(2,3), mean)[o,]
-	x$classprob.sd<- x$classprob.se<- apply(taustore, 2, sd)[o]
-	x$itemprob.sd<- x$itemprob.se<- apply(thetastore, c(2,3), sd)[o, ]
-	
-	if(G == 1){
-		 x$itemprob <- matrix(x$itemprob, G, M)
-		 x$itemprob.sd <- x$itemprob.se <- matrix(x$itemprob.sd, G, M)
-		 }
-	
-	if(G > 1)	dum<-array(apply(x$itemprob,1,dbinom, size=1, x=t(X)), dim=c(M,N,G)) else dum<-array( dbinom(t(X), 1, x$itemprob), dim=c(M,N,G))
-
-	Z1<-t(x$classprob*t(apply(dum, c(2,3), prod)))
-
-	x$logpost<- sum(log(rowSums(Z1))*counts.n) + sum(xlogy(alpha-1, x$itemprob) + xlogy(beta-1, 1- x$itemprob) + sum(xlogy(delta-1, x$classprob)))
-	
-	if(G > 1) x$Z<- (Zstore/counts.n)[, o] else x$Z<- matrix( (Zstore/counts.n)[, o], nrow = N, ncol = G)
-	
-	rownames(x$Z)<- names(counts.n)
-	colnames(x$Z)<- paste("Group", 1:G)
-	
-	x$samples<-NULL
-	if(G > 1){
-	x$samples$classprob<-taustore[, o]
-	x$samples$itemprob<-thetastore[, o, ]
-	} else {
-	x$samples$classprob<- taustore
-	x$samples$itemprob<- thetastore
-	}
-	x$samples$logpost<- logpost.store
-	
-	if(!is.null(colnames(X))){
-		colnames(x$itemprob)<- colnames(x$itemprob.sd)<- colnames(x$itemprob.se)<- colnames(X)
-		dimnames(x$samples$itemprob)<- list(NULL, NULL, colnames(X)) 
+		}
 	}
 	
-	Dbar<- mean(logpost.store)
-	S2<- var(logpost.store)
+	v.probs <- list()
+	tt <- seq( 0, stored-1, by=G )
+	l <- 1
+	for( j in 1:M )
+	{
+		if( model.indicator[j] )
+		{
+			v.probs[[l]] <- list()
+			v.probs[[l]]$mean  <- matrix( nrow = G, ncol = num.categories[j] ) 
+			v.probs[[l]]$se <- matrix( nrow = G, ncol = num.categories[j] )
+			for( g in 1:G )
+			{
+				b <- var.probs.l[[l]][ tt+g , ]
+				v.probs[[l]]$mean[g,] <- apply( b, 2, mean )
+				v.probs[[l]]$se[g,] <- apply( b, 2, sd )
+			}
+			l <- l+1
+		}
+	}
 	
-	x$DIC<- 2*(2*Dbar - x$logpost)
-	x$BICM<- 2*(Dbar - S2*(log(sum(counts.n))-1))
-	x$AICM<- 2*(Dbar - S2)
-	
-	x$counts<- counts.n
-	
-	x$prior<-NULL
-	x$prior$alpha<- alpha[o, ]
-	x$prior$beta<- beta[o, ]
-	x$prior$delta<- delta[o]
+	#compile the list to return
 
-	x$thin<-thin
-	x$burn.in<-burn.in
-	x$relabel<- relabel
-	x$labelstore<-labelstore
-
-	class(x)<-c("blca.gibbs", "blca")
-	#if(relabel && matchClasses(t(Z)%*%Z1, method="exact", verbose=FALSE)) warning("Label-switching (provisionally) corrected for - proceed with caution")
-	if(relabel && label.swap) warning("Label-switching (provisionally) corrected for - diagnostic plots are recommended. Use '?plot.blca' for details.")
-	if(!relabel && label.swap) warning("Label-switching may have occurred - diagnostic plots are recommended. Use '?plot.blca' for details.")
+	x <- list()
 	
-x
+	x$call <- match.call()
+	
+	x$classprob <- apply( weights.mat, 2, mean )
+	x$classprob.se <- apply( weights.mat, 2, sd )
+	
+	x$itemprob <- v.probs
+	
+	x$X <- X #this is needed for the post-hoc parameter step
+	x$num.categories <- num.categories
+	
+	x$samples <- list()
+	
+	x$samples$logpost <- w$log.posterior
+	x$samples$Giter <- rep( G, stored )
+	
+	if(relabel)
+	{
+		x$labelstore <- relabelled$relab
+		x$labelstore.permutation <- relabelled$permutation
+		x$G.Z <- relabelled$components
+		x$Z <- relabelled$label.prob
+	}
+	
+	#inputs
+	
+	x$iter <- iter
+	x$burn.in <- burn.in
+	x$thin <- thin
+	x$relabel <- relabel
+	x$prior <- list( alpha=alpha, beta=beta, delta=delta )
+	
+	t2 = proc.time()
+	
+	ttime = t2-t1
+  
+  	x$time = ttime[3]	
+  	
+  	class(x) <- c("blca.not.collapsed", "blca")
+	
+	return(x)
 }
